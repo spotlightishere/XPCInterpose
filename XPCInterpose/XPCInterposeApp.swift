1f3867c919cc3e3df087c107fcc7bf827123492f
Spotlight <spotlight@joscomputing.space>
2023-05-15 12:22:02 -0500

    Initial skeleton

---
+++ XPCInterpose/XPCInterposeApp.swift
@@ -0,0 +1,17 @@
+//
+//  XPCInterposeApp.swift
+//  XPCInterpose
+//
+//  Created by Spotlight Deveaux on 2023-05-15.
+//
+
+import SwiftUI
+
+@main
+struct XPCInterposeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
